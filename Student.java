a9659698d4612a934572ad0d658c94826c8958ee
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2024-08-21 09:20:34 +0530

    Third Commit
    Third Commit

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    public Student() {
+        System.out.println("Creating student object");
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hi , i am student");
+    }
+}
